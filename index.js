16536eb6ea1cf4169310ddf49495098918bf3688
mutemip <paulmutemi2030@gmail.com>
2022-06-24 21:23:36 +0300

    Creating and Inserting DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainDom = document.querySelector('main#main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Paul Mutemi is the champion";
+
