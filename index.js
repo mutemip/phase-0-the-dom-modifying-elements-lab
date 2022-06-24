5e7aceef137be1499bf1f8d8501c0aaa5d07ac28
mutemip <paulmutemi2030@gmail.com>
2022-06-24 21:32:34 +0300

    commit tree 8842e40e4679a69e2727d6cac5da026a1dfdaa60

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Write your code here!
-const mainDom = document.querySelector('main#main').remove();
+const mainDom = document.querySelector('main#main');
+mainDom.remove();
 
 const newHeader = document.createElement('h1');
 newHeader.id = "victory";
 
 newHeader.innerHTML = "Paul Mutemi is the champion";
-
+// newHeader.className = 'victory'
